566bcf7494bbf5d37fa83a172993f86aff0d7fab
Jason Tseng <jason.f.tseng@gmail.com>
2018-03-09 09:50:26 -0500

    deployed for prod

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,18 @@
+const utils = require
+
 module.exports = {
   baseUrl: './',
-  configureWebpack: config => {
-    rules: [
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
-          publicPath: process.env.NODE_ENV === 'production' ? '../../' : '/'
-        }
-      }
-    ]
-  }
+  // configureWebpack: config => {
+  //   rules: [
+  //     {
+  //       test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+  //       loader: 'url-loader',
+  //       options: {
+  //         limit: 10000,
+  //         name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+  //         publicPath: process.env.NODE_ENV === 'production' ? '../../' : '/'
+  //       }
+  //     }
+  //   ]
+  // }
 }
